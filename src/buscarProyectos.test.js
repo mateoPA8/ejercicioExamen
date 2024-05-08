bd178b3e86af51f37cdcdf01c46dbd8e2a330783
“mateopoquechoque” <mateo.poquechoque@ucb.edu.bo>
2024-05-08 15:34:38 -0400

    se encuentra mas de una coincidencia de un proyecto

--- src/buscarProyectos.test.js
+++ src/buscarProyectos.test.js
@@ -5,39 +5,40 @@
 // buscar y enceontrar mas de una coindencia de proyectos
 // buscar y enceontrar proyectos cuyo nombre empieza con el criterio de busqueda
 // por ejm: "ejerc" y en mi lista tengo "ejercio1", "ejercicio2" -> devuelve ambos
+function buscarProyecto(nombre, proyectos) 
+{
+    const coincidencias = [];
+    for (const proyecto of proyectos) {
+        if (proyecto === nombre) {
+            coincidencias.push(proyecto);
+        }
+    }
+    return coincidencias;
+}
+
+// Pruebas actualizadas:
 describe("Buscar", () => {
     it("no encuentra proyectos cuando no se tiene ninguno en la lista de proyectos", () => {     
-    let proyectos = [];
-     expect (buscarProyecto("ejercicio1", proyectos)).toEqual("");
-     });
-     it("encuentra un proyecto cuando el mismo existe en una lista de 1 proyecto", () => {
-        let proyectos = [];
-       proyectos.push("miUnicoProyecto");
-       expect (buscarProyecto("miUnicoProyecto", proyectos)).toEqual("miUnicoProyecto");
-       });
-       it("encuentra un proyecto cuando el mismo existe en una lista de varios proyectos", () => {
-        let proyectos = [];
-        proyectos.push("proyecto1");
-        proyectos.push("proyecto2");
-        proyectos.push("proyecto3");
-        expect (buscarProyecto("proyecto2", proyectos)).toEqual("proyecto2");
-        });
+        const proyectos = [];
+        expect(buscarProyecto("ejercicio1", proyectos)).toEqual([]);
+    });
+
+    it("encuentra un proyecto cuando el mismo existe en una lista de 1 proyecto", () => {
+        const proyectos = ["miUnicoProyecto"];
+        expect(buscarProyecto("miUnicoProyecto", proyectos)).toEqual(["miUnicoProyecto"]);
+    });
+
+    it("encuentra un proyecto cuando el mismo existe en una lista de varios proyectos", () => {
+        const proyectos = ["proyecto1", "proyecto2", "proyecto3"];
+        expect(buscarProyecto("proyecto2", proyectos)).toEqual(["proyecto2"]);
+    });
+
+    it("encuentra varias coincidencias de un proyecto", () => {
+        const proyectos = ["proyecto1", "proyecto1", "proyecto3"];
+        expect(buscarProyecto("proyecto1", proyectos)).toEqual(["proyecto1", "proyecto1"]);
+    });
 });
-function buscarProyecto(nombre,proyectos)
-{
-    if(nombre==proyectos[0])
-        {
-            return "miUnicoProyecto"
-        }
-    for(const proyecto of proyectos)
-        {
-            if(proyecto==nombre)
-                {
-                    return proyecto;
-                }
-        }
-    return "";
-}
+
           //Para terminar:
           
           // 1.- buscar y enceontrar mas de una coindencia de proyectos
